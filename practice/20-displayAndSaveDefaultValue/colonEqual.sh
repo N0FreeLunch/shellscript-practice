4e01c7fb9fb2248ece2973f743c1ba63307f7360
N0FreeLunch <consilience90@gmail.com>
2022-07-29 02:17:59 +0900

    variable:value description added

--- practice/20-displayAndSaveDefaultValue/colonEqual.sh
+++ practice/20-displayAndSaveDefaultValue/colonEqual.sh
@@ -4,3 +4,12 @@ echo ${OS_TYPE:-redhat}
 echo ${OS_TYPE}
 echo ${OS_TYPE:=redhat}
 echo ${OS_TYPE}
+
+
+OS_TYPE=""
+unset OS_TYPE
+
+echo ${OS_TYPE:-redhat}
+echo ${OS_TYPE}
+echo ${OS_TYPE:=redhat}
+echo ${OS_TYPE}
