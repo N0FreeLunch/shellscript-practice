2f9f139ce2bdd1040292b8c6276899d098d260db
N0FreeLunch <consilience90@gmail.com>
2022-08-05 03:59:07 +0900

    addOperation added

---
+++ practice/31-while/addOperation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+num=0
+
+num=$num+1
+
+echo $num;
+
+num=0
+
+num=($num+1)
+
+echo $num;
+
+num=0
+
+num=$((num+1))
+num=$((num+1))
+
+echo $num;
+
+
+num=0
+
+num=$(num+1)
+
+echo $num;
